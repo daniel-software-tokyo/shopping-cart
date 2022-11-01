c007e9a917a743a4cba53d8e3227461f529d8f9c
WATANABE Masahiro <watanabe@daniel-soft.com>
2022-11-01 09:42:41 +0900

    NUnitでどういう風にユニットテストを書けばいいのか、第一歩のサンプルコードを追記する

--- Bowling.Test/BowlingTest.vb
+++ Bowling.Test/BowlingTest.vb
@@ -2,4 +2,10 @@
 
 Public Class BowlingTest
 
+    <Test()> Public Sub NUnitでのサンプルコード()
+        Dim actual As Integer = 2 + 3
+
+        Assert.That(actual, [Is].EqualTo(5))
+    End Sub
+
 End Class
